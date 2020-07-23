0f05ace3767eda0711123b73c7803c03e9bdaeba
Ray Weitzenberg <ray@mechaneyes.com>
2020-07-23 16:23:00 -0400

    bears's ears

---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import HomeHero from '../../components/atoms/HomeHero/HomeHero'
+
+function Homepage() {
+    return (
+        <div>
+            <HomeHero />
+        </div>
+    )
+}
+
+export default Homepage
