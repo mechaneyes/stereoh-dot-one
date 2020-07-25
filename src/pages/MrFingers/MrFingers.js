df1bd6033f462b7b6ba74c74a8f7d2abe302aee4
Ray Weitzenberg <ray@mechaneyes.com>
2020-07-24 23:02:41 -0400

    mrFingers: paging through additional content
    clicking through the nav in the 'related' area reveals different
    content. lotta duplicate code, but it works. needs serious refactoring

--- src/pages/MrFingers/MrFingers.js
+++ src/pages/MrFingers/MrFingers.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 // import EventInfo from "../../components/atoms/EventInfo/EventInfo";
 import GradientHero from "../../components/atoms/GradientHero/GradientHero";
@@ -8,48 +8,108 @@ import "./MrFingers.scss";
 
 const imgPath = process.env.PUBLIC_URL + "/assets/";
 
-export class MrFingers extends Component {
-  render() {
-    const defaultProps = {
-      title: defaultValues.HERO_TITLE,
-      subhead: defaultValues.SUBHEADLINE_SHORT,
-    };
+const defaultProps = {
+  title: defaultValues.HERO_TITLE,
+  subhead: defaultValues.SUBHEADLINE_SHORT,
+};
 
-    const relatedItems = [
-      {
-        relImage: `${imgPath}jenAndMe.png`,
-        relTitle: "It's just (House of Dupree)",
-        relSubhead: defaultProps.subhead,
-        relLinkUrl: "#",
-      },
-      {
-        relImage: `${imgPath}tropicalFreeze_066.png`,
-        relTitle: "Pier Children",
-        relSubhead: defaultProps.subhead,
-        relLinkUrl: "#",
-      },
-      {
-        relImage: `${imgPath}tropicalFreeze_051.png`,
-        relTitle: "Inside the Deku Tree",
-        relSubhead: defaultProps.subhead,
-        relLinkUrl: "#",
-      },
-    ];
+const MrFingers = () => {
+  const [extras, setExtras] = useState(0)
 
-    const relatedList = relatedItems.map((rel, i) => (
-      <RelatedItem key={i} {...rel} />
-    ));
+  let shownItems = []  
+  
 
-    return (
-      <div className="single mr-fingers">
-        <GradientHero />
-        <section className="related-content">
-          <h2>Earlier</h2>
-          <div className="related-grid">{relatedList}</div>
-        </section>
-      </div>
-    );
+  const earlierItems = [
+    {
+      relImage: `${imgPath}jenAndMe.png`,
+      relTitle: "It's just (House of Dupree)",
+      relSubhead: defaultProps.subhead,
+      relLinkUrl: "#",
+    },
+    {
+      relImage: `${imgPath}tropicalFreeze_066.png`,
+      relTitle: "Pier Children",
+      relSubhead: defaultProps.subhead,
+      relLinkUrl: "#",
+    },
+    {
+      relImage: `${imgPath}tropicalFreeze_051.png`,
+      relTitle: "Inside the Deku Tree",
+      relSubhead: defaultProps.subhead,
+      relLinkUrl: "#",
+    },
+  ];
+
+  const alsoItems = [
+    {
+      relImage: `${imgPath}tropicalFreeze_066.png`,
+      relTitle: "Pier Children",
+      relSubhead: defaultProps.subhead,
+      relLinkUrl: "#",
+    },
+    {
+      relImage: `${imgPath}tropicalFreeze_051.png`,
+      relTitle: "Inside the Deku Tree",
+      relSubhead: defaultProps.subhead,
+      relLinkUrl: "#",
+    },
+    {
+      relImage: `${imgPath}jenAndMe.png`,
+      relTitle: "It's just (House of Dupree)",
+      relSubhead: defaultProps.subhead,
+      relLinkUrl: "#",
+    },
+  ];
+
+  const detailsItems = [
+    {
+      relImage: `${imgPath}tropicalFreeze_051.png`,
+      relTitle: "Inside the Deku Tree",
+      relSubhead: defaultProps.subhead,
+      relLinkUrl: "#",
+    },
+    {
+      relImage: `${imgPath}jenAndMe.png`,
+      relTitle: "It's just (House of Dupree)",
+      relSubhead: defaultProps.subhead,
+      relLinkUrl: "#",
+    },
+    {
+      relImage: `${imgPath}tropicalFreeze_066.png`,
+      relTitle: "Pier Children",
+      relSubhead: defaultProps.subhead,
+      relLinkUrl: "#",
+    },
+  ];
+
+  if (extras === 0) {
+    shownItems = earlierItems
+  } else if (extras === 1) {
+    shownItems = alsoItems
+  } else if (extras === 2) {
+    shownItems = detailsItems
   }
-}
+
+  const relatedList = shownItems.map((rel, i) => (
+    <RelatedItem key={i} {...rel} />
+  ));
+
+  return (
+    <div className="single mr-fingers">
+      <GradientHero />
+      <nav className="related-nav">
+        <ul>
+          <li onClick={() => setExtras(0)}>earlier</li>
+          <li onClick={() => setExtras(1)}>you may also like</li>
+          <li onClick={() => setExtras(2)}>details</li>
+          <li>{extras}</li>
+        </ul>
+      </nav>
+      <section className="related-content">
+        <div className="related-grid">{relatedList}</div>
+      </section>
+    </div>
+  );
+};
 
 export default MrFingers;
