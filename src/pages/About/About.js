c108bcf20d30b8f6743609a646bb82ebcee13173
Ray Weitzenberg <ray@mechaneyes.com>
2020-09-11 22:22:45 -0400

    about page to heroku

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,37 +1,38 @@
 import React from "react";
 
 import EventHero from "../../components/atoms/EventHero/EventHero";
-import PullImgLeft from "../../components/atoms/PullQuotes/PullImgLeft";
-import PullImgRight from "../../components/atoms/PullQuotes/PullImgRight";
 import RelatedItems from "../../components/molecules/RelatedItems/RelatedItems";
 import "./About.scss";
 
 const imgPath = process.env.PUBLIC_URL + "/assets/";
 
 const Neoneon = () => {
-  const quoteOne = {
-    pullImg: `${imgPath}neoneon_002.jpg`,
-  };
-
-  const quoteTwo = {
-    pullImg: `${imgPath}neoneon_004.jpg`,
-  };
 
   document.title = "Stereoh | About";
 
   return (
     <div className="single anima">
-      <EventHero
-        homeHeroImg={imgPath + "barclays.jpg"}
-        headline="About"
-        subheadline="Stereoh is a platform for live music shows and interactive experiences
-        in a novel format that combines streaming video with freely
-        traversable social 3D environments, where audience members can talk
-        and interact naturally, and artists can meet their fans personally. We
-        are constantly adding new functionality and plotting new experiences,
-        so keep in touch /hyperlink to signup/ and drop by to check out the
-        latest events."
-      />
+
+      <section className="event-hero">
+        <img
+          className="home-hero-img"
+          src={imgPath + "barclays.jpg"}
+          alt="neonPlatform hero"
+        />
+        <div className="home-hero-content">
+          <h1>About</h1>
+          <p className="home-hero-subheadline">
+            Stereoh is a platform for live music shows and interactive
+            experiences in a novel format that combines streaming video with
+            freely traversable social 3D environments, where audience members
+            can talk and interact naturally, and artists can meet their fans
+            personally. We are constantly adding new functionality and plotting
+            new experiences, so keep in touch /hyperlink to signup/ and drop by
+            to check out the latest events.
+          </p>
+        </div>
+        <div className="gradient"></div>
+      </section>
 
       <nav className="related-nav">
         <ul>
