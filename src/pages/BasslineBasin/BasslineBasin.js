5e4f54475d805f60816cac9a6cfe74c3415c640b
Ray Weitzenberg <ray@mechaneyes.com>
2020-09-18 09:56:15 -0400

    more content moving above the fold

--- src/pages/BasslineBasin/BasslineBasin.js
+++ src/pages/BasslineBasin/BasslineBasin.js
@@ -9,6 +9,8 @@ import "./EventHero.scss";
 
 const imgPath = process.env.PUBLIC_URL + "/assets/";
 
+document.title = "Stereoh | neoneon";
+
 const eventInfo = {
   title: "Bassline Basin",
   subHeadline: "Corrosive environment filled with radioactive beats",
@@ -29,10 +31,8 @@ const Neoneon = () => {
     pullImg: `${imgPath}neoneon_004.jpg`,
   };
 
-  document.title = "Stereoh | neoneon";
-
   return (
-    <div className="single anima">
+    <div className="single bassline">
       <section className="event-hero">
         <img
           className="home-hero-img"
@@ -43,26 +43,28 @@ const Neoneon = () => {
         <div className="home-hero-content">
           <EventInfo {...eventInfo} />
         </div>
-        
+
         <div className="gradient"></div>
       </section>
 
-      <PullImgLeft {...quoteOne} />
+      <section className="bassline-body">
+        <PullImgLeft {...quoteOne} />
 
-      <section className="body-copy">
-        <p>
-          Left behind by an extinct civilization of advanced space-farers is a
-          planetary formation traversable only in full protective gear. Shrouded
-          by corrosive atmosphere here lies an acid lake surrounded by remnants
-          of alien technology. Jagged basslines and abstract signals are
-          broadcast from the hard core of this destination for the adventurous
-          travelers.
-        </p>
+        <section className="body-copy">
+          <p>
+            Left behind by an extinct civilization of advanced space-farers is a
+            planetary formation traversable only in full protective gear.
+            Shrouded by corrosive atmosphere here lies an acid lake surrounded
+            by remnants of alien technology. Jagged basslines and abstract
+            signals are broadcast from the hard core of this destination for the
+            adventurous travelers.
+          </p>
+        </section>
+
+        <PullImgRight {...quoteTwo} />
+
+        <RelatedItems hide="neoneon" />
       </section>
-
-      <PullImgRight {...quoteTwo} />
-
-      <RelatedItems hide="neoneon" />
     </div>
   );
 };
